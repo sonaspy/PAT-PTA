bbfe796209d1a85da22eed86eb351a6867cec30f
sonaspy <sonaspy@zju.edu.cn>
2018-12-14 15:29:07 +0800

    35

--- DST_CN/7-35.cpp
+++ DST_CN/7-35.cpp
@@ -1,38 +1,15 @@
-// author - newguo@sonaspy.cn
-// coding - utf_8
+// author - newguo@sonaspy.cn 
+// coding - utf_8 
 
-#include <iostream>
-#include <cstring>
-#include <cstdlib>
-#define test() freopen("in", "r", stdin)
+#include<iostream>
+
+#define test() freopen("in","r",stdin)
 
 using namespace std;
-
-double preExp();
 
 int main(int argc, char const *argv[])
 {
     /* code */
-    printf("%.1f", preExp());
-    return 0;
-}
 
-double preExp()
-{
-    char s[20];
-    cin >> s;
-    switch (s[0])
-    {
-    case '+':
-        return preExp() + preExp();
-    case '-':
-        return preExp() - preExp();
-    case '/':
-        return preExp() / preExp();
-    case '*':
-        return preExp() * preExp();
-    default:
-        return atof(s);
-        break;
-    }
+    return 0;
 }

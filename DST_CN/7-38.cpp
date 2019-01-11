c191a18d58341b6f0376908b34c488a98db8dc71
sonaspy <sonaspy@zju.edu.cn>
2019-01-11 17:13:24 +0800

    update

---
+++ DST_CN/7-38.cpp
@@ -0,0 +1,29 @@
+// author - newguo@sonaspy.cn 
+// coding - utf_8 
+
+#include<iostream>
+#include<algorithm>
+#define test() freopen("in","r",stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    test();
+    int N, M, richer[1000001];
+    cin >> N >> M;
+    for(int i = 0; i < N; i++)
+        scanf("%d",&richer[i]);
+    sort(richer,richer+N,greater<int>());
+    printf("%d",richer[0]);
+    if(N >= M){
+        for(int i = 1; i < M; i++)
+            printf(" %d", richer[i]);
+    }
+    else{
+        for (int i = 1; i < N; i++)
+            printf(" %d", richer[i]);
+    }
+    return 0;
+}

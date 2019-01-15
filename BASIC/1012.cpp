023fa7963e8ecf17ca29e592b43a06d2a13e78ce
sonaspy <sonaspy@zju.edu.cn>
2019-01-15 22:53:31 +0800

    1012

--- BASIC/1012.cpp
+++ BASIC/1012.cpp
@@ -13,44 +13,46 @@ int main(int argc, char const *argv[])
     //test();
     int n, num, sum = 0, f = -1, a1 = 0, a2 = 0, a3 = 0, a5 = 0,count = 0;
     double a4;
+    bool s = false;
     cin >> n;
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &num);
         if (num % 5 == 0 && num % 2 == 0)
             a1 += num;
-        if (num % 5 == 1)
+        else if (num % 5 == 1)
         {
             f *= -1;
-            a2 += f * num;
+            s = true;
+            a2 += (f * num);
         }
-        if (num % 5 == 2)
+        else if (num % 5 == 2)
             a3 += 1;
-        if (num % 5 == 3){
+        else if (num % 5 == 3){
             sum += num;
             count += 1;
         }
-        if (num % 5 == 4 && num > a5)
+        else if (num % 5 == 4 && num > a5)
             a5 = num;
     }
     a4 = sum * 1.0 / count;
-    if (a1)
+    if (a1 != 0)
         printf("%d", a1);
     else
         printf("N");
-    if (a2)
+    if (s)
         printf(" %d", a2);
     else
         printf(" N");
-    if (a3)
+    if (a3!=0)
         printf(" %d", a3);
     else
         printf(" N");
-    if (count)
+    if (count!=0)
         printf(" %.1f", a4);
     else
         printf(" N");
-    if (a5)
+    if (a5!= 0)
         printf(" %d", a5);
     else
         printf(" N");

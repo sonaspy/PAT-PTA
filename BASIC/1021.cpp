fc32a4d3ea9351e44741355af7645d0217169706
sonaspy <sonaspy@zju.edu.cn>
2019-01-16 06:23:41 +0800

    1021/1022

--- BASIC/1021.cpp
+++ BASIC/1021.cpp
@@ -2,7 +2,8 @@
 // coding - utf_8 
 
 #include<iostream>
-
+#include<map>
+#include<string>
 #define test() freopen("in","r",stdin)
 
 using namespace std;
@@ -11,6 +12,14 @@ int main(int argc, char const *argv[])
 {
     /* code */
     test();
-    
+    map<char,int> mp;
+    string s;
+    cin >> s;
+    for(auto iter : s){
+        mp[iter] += 1;
+    }
+    for(auto iter: mp){
+        printf("%c:%d\n",iter.first,iter.second);
+    }
     return 0;
 }
---
+++ BASIC/1022.cpp
@@ -0,0 +1,33 @@
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <iostream>
+#include<string>
+#include<algorithm>
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    //test();
+    int a, b, c, d, sh, r;
+    cin >> a >> b >> d;
+    c = a + b;
+    string s;
+    sh = c / d;
+    r = c % d;
+    while (sh || r)
+    {   
+        s.push_back(r+'0');
+        r = sh % d;
+        sh /= d;
+    }
+    reverse(s.begin(),s.end());
+    if (!c)
+        cout << 0;
+    else
+        cout << s;
+    return 0;
+}

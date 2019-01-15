d230bdef831fe72c9b950eb929685b93ccd8cf9a
sonaspy <sonaspy@zju.edu.cn>
2019-01-15 20:28:43 +0800

    1008

---
+++ BASIC/1008.cpp
@@ -0,0 +1,37 @@
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <iostream>
+#include <vector>
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    test();
+    vector<int> a, b;
+    int n, m, flag = 1, tmp;
+    cin >> n >> m;
+    m %= n;
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &tmp);
+        a.push_back(tmp);
+    }
+    b.resize(a.size());
+    copy(a.begin() + a.size() - m, a.end(), b.begin());
+    copy(a.begin(), a.begin() + a.size() - m, b.begin() + m);
+    for (auto iter : b)
+    {
+        if (flag)
+        {
+            flag = 0;
+            printf("%d", iter);
+        }
+        else
+            printf(" %d", iter);
+    }
+    return 0;
+}

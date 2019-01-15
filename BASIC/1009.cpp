07b5e41988a69fc90bfcb401629e8df77cbc4e1c
sonaspy <sonaspy@zju.edu.cn>
2019-01-15 20:38:24 +0800

    1009/1010

---
+++ BASIC/1009.cpp
@@ -0,0 +1,30 @@
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <iostream>
+#include <string>
+#include <list>
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    test();
+    list<string> container;
+    string s;
+    int flag = 1;
+    while(cin >> s)
+        container.push_front(s);
+    for(auto iter : container){
+        if (flag)
+        {
+            flag = 0;
+            cout << iter;
+        }
+        else
+            cout << " " << iter;
+    }
+    return 0;
+}
---
+++ BASIC/1010.cpp
@@ -0,0 +1,43 @@
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <iostream>
+#include <vector>
+
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    // test();
+    int n1, n2;
+    vector<int> res;
+    while (cin >> n1 >> n2)
+    {
+        if (n2 != 0)
+        {
+            res.push_back(n1 * n2);
+            res.push_back(n2 - 1);
+        }
+    }
+    if (!res.size())
+        printf("0 0");
+    else
+    {
+        bool f = true;
+        for (auto iter : res)
+        {
+            if (f)
+            {
+                printf("%d", iter);
+                f = false;
+            }
+            else
+                printf(" %d", iter);
+        }
+    }
+
+    return 0;
+}

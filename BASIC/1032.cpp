822694e0fa1a6346449e250c56cb3793d386208b
sonaspy <sonaspy@zju.edu.cn>
2019-01-17 19:52:20 +0800

    1032

--- BASIC/1032.cpp
+++ BASIC/1032.cpp
@@ -1,16 +1,34 @@
-// author - newguo@sonaspy.cn 
-// coding - utf_8 
+// author - newguo@sonaspy.cn
+// coding - utf_8
 
-#include<iostream>
-
-#define test() freopen("in","r",stdin)
+#include <iostream>
+#include <algorithm>
+#define test() freopen("in", "r", stdin)
 
 using namespace std;
 
+struct Node
+{
+    int id;
+    int score = 0;
+} nodes[100001];
+bool cmp(struct Node A, struct Node B)
+{
+    return A.score > B.score;
+}
 int main(int argc, char const *argv[])
 {
     /* code */
-    test();
-    
+    // test();
+    int n, id, score;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d%d", &id, &score);
+        nodes[id].id = id;
+        nodes[id].score += score;
+    }
+    sort(nodes + 1, nodes + n + 1, cmp);
+    cout << nodes[1].id << " " << nodes[1].score;
     return 0;
 }

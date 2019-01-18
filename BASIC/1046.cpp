7b251adc33dfe855c719a35385356f94160579da
sonaspy <sonaspy@zju.edu.cn>
2019-01-18 17:42:58 +0800

    1045/1046

---
+++ BASIC/1046.cpp
@@ -0,0 +1,27 @@
+// author - newguo@sonaspy.cn 
+// coding - utf_8 
+
+#include<iostream>
+
+#define test() freopen("in","r",stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    test();
+    int n, jhan,jhua,yhan,yhua, cnt1 = 0,cnt2 = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> jhan >> jhua >> yhan >> yhua;
+        if ((jhua == jhan + yhan && yhua == yhan + jhan) || (jhua != jhan + yhan && yhua != yhan + jhan))
+            continue;
+        else if (jhua == jhan + yhan && yhua != yhan + jhan)
+            cnt2++;
+        else if (jhua != jhan + yhan && yhua == yhan + jhan)
+            cnt1++;
+    }
+    cout << cnt1 << " " << cnt2;
+    return 0;
+}

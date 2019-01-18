fb0611ee24d250a27007ec84ff15d78049949d5f
sonaspy <sonaspy@zju.edu.cn>
2019-01-18 19:33:18 +0800

    1047/1048

---
+++ BASIC/1047.cpp
@@ -0,0 +1,41 @@
+// author - newguo@sonaspy.cn 
+// coding - utf_8 
+
+#include<iostream>
+#include<vector>
+#include <algorithm>
+#include<string.h>
+#define test() freopen("in","r",stdin)
+
+using namespace std;
+
+struct Node{
+    int id = 0, total = 0;
+}nodes[1001];
+
+bool cmp(struct Node A, struct Node B){
+    return A.total > B.total;
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    test();
+    int n, score, id;
+    cin >>n;
+    const char *sep  = "-";
+    char s[10], *s1;
+    for(int i = 0; i < n ; i++){
+        scanf("%s%d",s,&score);
+        s1 = strtok(s, sep);
+        id = atoi(s1);
+        nodes[id].id = id;
+        nodes[id].total += score;
+    }
+    sort(nodes, nodes+1001,cmp);
+    cout << nodes[0].id << " " << nodes[0].total;
+
+
+
+    return 0;
+}
---
+++ BASIC/1048.cpp
@@ -0,0 +1,43 @@
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    string A, B, result;
+    cin >> A >> B;
+    reverse(A.begin(), A.end());
+    reverse(B.begin(), B.end());
+    if (A.length() < B.length())
+        A += string(B.length() - A.length(), '0');
+    else if (A.length() > B.length())
+        B += string(A.length() - B.length(), '0');
+    int len = B.length();
+    for (int i = 0; i < len; i++)
+    {
+        if (i % 2 == 0)
+        {
+            int temp = (A[i] + B[i] - '0' - '0') % 13;
+            if (temp == 10)
+                result += 'J';
+            else if (temp == 11)
+                result += 'Q';
+            else if (temp == 12)
+                result += 'K';
+            else
+                result += temp + '0';
+        }
+        else
+            result += (B[i] - A[i] + 10) % 10 + '0';
+    }
+    reverse(result.begin(), result.end());
+    cout << result;
+    return 0;
+}

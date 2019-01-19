f4b932dc73c7987488a9e1a965ce290be9bf7036
sonaspy <sonaspy@zju.edu.cn>
2019-01-19 23:15:09 +0800

    1069

--- BASIC/1069.cpp
+++ BASIC/1069.cpp
@@ -1,9 +1,10 @@
-// author - newguo@sonaspy.cn 
-// coding - utf_8 
+// author - newguo@sonaspy.cn
+// coding - utf_8
 
-#include<iostream>
-
-#define test() freopen("in","r",stdin)
+#include <iostream>
+#include <unordered_set>
+#include <string>
+#define test() freopen("in", "r", stdin)
 
 using namespace std;
 
@@ -11,5 +12,29 @@ int main(int argc, char const *argv[])
 {
     /* code */
     test();
+    int m = 0, n = 0, s = 0;
+    string str[1001];
+    unordered_set<string> st;
+    cin >> m >> n >> s;
+    for (int i = 1; i <= m; i++)
+        cin >> str[i];
+    if (s > m)
+    {
+        cout << "Keep going...";
+        return 0;
+    }
+    else
+        for (int i = s; i <= m;)
+        {
+
+            if (!st.count(str[i]))
+            {
+                cout << str[i] << endl;
+                st.insert(str[i]);
+                i += n;
+            }
+            else
+                i++;
+        }
     return 0;
 }

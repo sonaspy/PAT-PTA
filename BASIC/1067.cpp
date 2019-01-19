97a1eadc917bc0f5832364312c4dc59a84c2b600
sonaspy <sonaspy@zju.edu.cn>
2019-01-19 20:56:05 +0800

    1067

--- BASIC/1067.cpp
+++ BASIC/1067.cpp
@@ -1,9 +1,9 @@
-// author - newguo@sonaspy.cn 
-// coding - utf_8 
+// author - newguo@sonaspy.cn
+// coding - utf_8
 
-#include<iostream>
-
-#define test() freopen("in","r",stdin)
+#include <iostream>
+#include <string>
+#define test() freopen("in", "r", stdin)
 
 using namespace std;
 
@@ -11,5 +11,25 @@ int main(int argc, char const *argv[])
 {
     /* code */
     test();
+    string correct, trypwd;
+    int n, count = 1;
+    cin >> correct >> n;
+    getchar();
+    while (getline(cin, trypwd), trypwd != "#")
+    {
+        if (trypwd == correct)
+        {
+            printf("Welcome in\n");
+            break;
+        }
+        else
+            printf("Wrong password: %s\n", trypwd.c_str());
+        if (count == n)
+        {
+            printf("Account locked\n");
+            break;
+        }
+        count++;
+    }
     return 0;
 }

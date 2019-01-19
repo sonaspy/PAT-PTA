863dfb95a029bd736cb62d0afadb0d625888f6b6
sonaspy <sonaspy@zju.edu.cn>
2019-01-19 18:09:01 +0800

    1060

--- BASIC/1060.cpp
+++ BASIC/1060.cpp
@@ -1,9 +1,10 @@
-// author - newguo@sonaspy.cn 
-// coding - utf_8 
+// author - newguo@sonaspy.cn
+// coding - utf_8
 
-#include<iostream>
-
-#define test() freopen("in","r",stdin)
+#include <iostream>
+#include <map>
+#include <algorithm>
+#define test() freopen("in", "r", stdin)
 
 using namespace std;
 
@@ -11,5 +12,20 @@ int main(int argc, char const *argv[])
 {
     /* code */
     test();
+    int n;
+    scanf("%d", &n);
+    int a[n + 1];
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &a[i]);
+    sort(a + 1, a + n + 1, greater<int>());
+    int ans = 0;
+    int p = 1;
+    while (ans <= n && a[p] > p)
+    {
+        ans++;
+        p++;
+    }
+    printf("%d", ans);
+
     return 0;
 }

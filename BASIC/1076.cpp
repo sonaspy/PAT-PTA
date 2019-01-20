eab845bd8d4b12878487a371be67fc0755b6fb94
sonaspy <sonaspy@zju.edu.cn>
2019-01-21 00:21:42 +0800

    1076

---
+++ BASIC/1076.cpp
@@ -0,0 +1,31 @@
+// author - newguo@sonaspy.cn 
+// coding - utf_8 
+
+#include<iostream>
+#include<string>
+#define test() freopen("in","r",stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    test(); 
+    int n;
+    cin >> n;
+    for(int i = 0; i < n ; i++){
+            string s;
+        for(int j = 0;j < 4; j++){
+            cin >> s;
+            if(s == "A-T")
+                printf("1");
+            else if(s == "B-T")
+                printf("2");
+            else if (s == "C-T")
+                printf("3");
+            else if (s == "D-T")
+                printf("4");
+        }
+    }
+    return 0;
+} 

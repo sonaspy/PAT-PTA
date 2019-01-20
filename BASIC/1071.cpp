d2f065528c444cea8ad06f3e29c192c4e2afc81c
sonaspy <sonaspy@zju.edu.cn>
2019-01-20 18:00:28 +0800

    1071/1072

---
+++ BASIC/1071.cpp
@@ -0,0 +1,39 @@
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <iostream>
+
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    test();
+    int T, K;
+    cin >> T >> K;
+    for (int i = 0; i < K; i++)
+    {
+        int n1, b, t, n2;
+        cin >> n1 >> b >> t >> n2;
+        if (t > T)
+            printf("Not enough tokens. Total = %d.\n", T);
+        else if ((n1 < n2 && b) || (n1 > n2 && !b))
+        {
+            T += t;
+            printf("Win %d! Total = %d.\n", t, T);
+        }
+        else if ((n1 < n2 && !b) || (n1 > n2 && b))
+        {
+            T -= t;
+            printf("Lose %d. Total = %d.\n", t, T);
+            if (T == 0)
+            {
+                printf("Game Over.\n");
+                break;
+            }
+        }
+    }
+    return 0;
+}
---
+++ BASIC/1072.cpp
@@ -0,0 +1,50 @@
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <iostream>
+#include <unordered_set>
+#include <string>
+#include <vector>
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    test();
+    int n, m, id, sum_stu = 0, sum_g = 0;
+    unordered_set<int> st;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        cin >> id;
+        st.insert(id);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        string name;
+        cin >> name;
+        cin >> num;
+        vector<int> v;
+        for (int j = 0; j < num; j++)
+        {
+            cin >> id;
+            if (st.count(id))
+                v.push_back(id);
+        }
+        if (v.size() > 0)
+        {
+            sum_stu += 1;
+            sum_g += v.size();
+            cout << name << ":";
+            for (auto iter : v)
+                printf(" %04d", iter);
+            printf("\n");
+        }
+    }
+    printf("%d %d", sum_stu, sum_g);
+
+    return 0;
+}

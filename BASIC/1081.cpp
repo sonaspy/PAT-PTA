6037038d959db4a5b64796298617cc2e3e45ce20
sonaspy <sonaspy@zju.edu.cn>
2019-01-21 15:33:10 +0800

    108x

---
+++ BASIC/1081.cpp
@@ -0,0 +1,54 @@
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <iostream>
+#include <string>
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    test();
+    string s;
+    int n;
+    cin >> n;
+    getchar();
+    for (int i = 0; i < n; i++)
+    {
+        bool hasAlpha = false, hasDigit = false, tailuan = false;
+        getline(cin , s);
+        if (s.size() < 6)
+        {
+            printf("Your password is tai duan le.\n");
+            continue;
+        }
+        else
+        {
+            for (auto iter : s)
+            {
+                if (!isalpha(iter) && iter != '.' && !(iter >= '0' && iter <= '9'))
+                {
+                    cout << "Your password is tai luan le.\n";
+                    tailuan = true;
+                    break;
+                }
+                if (isalpha(iter))
+                    hasAlpha = true;
+                else if (iter >= '0' && iter <= '9')
+                    hasDigit = true;
+            }
+            if (!tailuan)
+            {
+                if (!hasDigit)
+                    printf("Your password needs shu zi.\n");
+                else if (!hasAlpha)
+                    printf("Your password needs zi mu.\n");
+                else
+                    printf("Your password is wan mei.\n");
+            }
+        }
+    }
+    return 0;
+}
---
+++ BASIC/1082.cpp
@@ -0,0 +1,38 @@
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <iostream>
+#include <cmath>
+#include <string>
+
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    test();
+    int min = 10000000, max = -1, a1, a2, n;
+    string first, last, s;
+    double distance = 0.0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s >> a1 >> a2;
+        distance = pow(abs(a1) * abs(a1) + abs(a2) * abs(a2), 0.5);
+        if (distance > max)
+        {
+            max = distance;
+            first = s;
+        }
+        if (distance < min)
+        {
+            min = distance;
+            last = s;
+        }
+    }
+    cout << last << " " << first;
+
+    return 0;
+}

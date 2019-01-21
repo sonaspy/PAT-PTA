671ea934e0bd61457e419a6e68cc61be258e4354
sonaspy <sonaspy@zju.edu.cn>
2019-01-21 15:47:09 +0800

    1083

--- BASIC/1083.cpp
+++ BASIC/1083.cpp
@@ -1,9 +1,10 @@
-// author - newguo@sonaspy.cn 
-// coding - utf_8 
+// author - newguo@sonaspy.cn
+// coding - utf_8
 
-#include<iostream>
-
-#define test() freopen("in","r",stdin)
+#include <iostream>
+#include <map>
+#include <cmath>
+#define test() freopen("in", "r", stdin)
 
 using namespace std;
 
@@ -11,5 +12,19 @@ int main(int argc, char const *argv[])
 {
     /* code */
     test();
+    int n, num, d_value;
+    map<int, int, greater<int>> mp;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> num;
+        d_value = abs(num - i);
+        mp[d_value] += 1;
+    }
+    for (auto iter : mp)
+    {
+        if (iter.second != 1)
+            cout << iter.first << " " << iter.second << endl;
+    }
     return 0;
 }

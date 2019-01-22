1040f8a97f0b330631f50ed2a76c1a4af9450de4
sonaspy <sonaspy@zju.edu.cn>
2019-01-22 19:28:17 +0800

    6-9

---
+++ DST_EN/6-9.h
@@ -0,0 +1,19 @@
+void MySort(ElementType A[], int N)
+{
+    int i = 0;
+    int Front = 0;
+    int Rear = N - 1;
+    ElementType *B = (ElementType *)malloc(N * sizeof(ElementType));
+    for (i = 0; i < N; i++)
+    {
+        if (A[i] == false)
+            B[Front++] = A[i];
+        if (A[i] == true)
+            B[Rear--] = A[i];
+    }
+    for (i = Front; i <= Rear; i++)
+        B[i] = maybe;
+    for (i = 0; i < N; i++)
+        A[i] = B[i];
+    free(B);
+}

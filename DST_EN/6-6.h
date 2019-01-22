2fffa090b916286425ff15a50a4af8df9d3b7095
sonaspy <sonaspy@zju.edu.cn>
2019-01-22 18:55:33 +0800

    6-6

---
+++ DST_EN/6-6.h
@@ -0,0 +1,15 @@
+void Level_order(Tree T, void (*visit)(Tree ThisNode))
+{
+    Tree Queue[100];
+    int Head = 0, Next = 0;
+    if (T != NULL)
+        Queue[Next++] = T;
+    while (Head < Next)
+    {
+        if (Queue[Head]->Left)
+            Queue[Next++] = Queue[Head]->Left;
+        if (Queue[Head]->Right)
+            Queue[Next++] = Queue[Head]->Right;
+        visit(Queue[Head++]);
+    }
+}

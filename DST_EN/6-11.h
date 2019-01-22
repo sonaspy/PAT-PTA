c31d9560197ac847ab03ded43a4157fc26084e84
sonaspy <sonaspy@zju.edu.cn>
2019-01-22 20:19:44 +0800

    6-11

---
+++ DST_EN/6-11.h
@@ -0,0 +1,26 @@
+void ShortestDist(LGraph Graph, int dist[], Vertex S)
+{
+    int front = 0, rear = 0;
+    PtrToAdjVNode p = NULL;
+    Vertex *que = (Vertex *)malloc(MaxVertexNum * sizeof(Vertex));
+    for (int i = 0; i < MaxVertexNum; i++)
+        que[i] = dist[i] = -1;
+    dist[S] = 0;
+    que[0] = S;
+
+    while (front <= rear)
+    {
+        Vertex s = que[front++];
+        p = Graph->G[s].FirstEdge;
+        while (p)
+        {
+            Vertex v = p->AdjV;
+            if (dist[v] == -1)
+            {
+                dist[v] = 1 + dist[s];
+                que[++rear] = v;
+            }
+            p = p->Next;
+        }
+    }
+}

712f5c647a3a775572010a6b784053d40aec3f0f
sonaspy <sonaspy@zju.edu.cn>
2019-01-22 20:36:37 +0800

    6-12

--- DST_EN/6-12.h
+++ DST_EN/6-12.h
@@ -1,22 +1,21 @@
-
 void ShortestDist(MGraph Graph, int dist[], Vertex S)
 {
-    int visited[MaxVertexNum];
+    int book[MaxVertexNum];
     int i, j, k;
     for (j = 0; j < Graph->Nv; j++)
     {
-        visited[j] = 0;
+        book[j] = 0;
         dist[j] = Graph->G[S][j];
     }
-    visited[S] = 1;
+    book[S] = 1;
     dist[S] = 0;
-    for (j = 0; j < Graph->Nv - 1; j++)
+    for (j = 0; j < Graph->Nv; j++)
     {
         int min = INFINITY;
         int u = -1;
         for (k = 0; k < Graph->Nv; k++)
         {
-            if (visited[k] == 0 && dist[k] < min)
+            if (book[k] == 0 && dist[k] < min)
             {
                 min = dist[k];
                 u = k;
@@ -24,13 +23,11 @@ void ShortestDist(MGraph Graph, int dist[], Vertex S)
         }
         if (u == -1)
             break;
-        visited[u] = 1;
+        book[u] = 1;
         for (k = 0; k < Graph->Nv; k++)
         {
-            if (!visited[k] && dist[k] > dist[u] + Graph->G[u][k])
-            {
+            if (!book[k] && dist[k] > dist[u] + Graph->G[u][k])
                 dist[k] = dist[u] + Graph->G[u][k];
-            }
         }
     }
     for (i = 0; i < Graph->Nv; i++)
@@ -38,4 +35,4 @@ void ShortestDist(MGraph Graph, int dist[], Vertex S)
         if (dist[i] == INFINITY)
             dist[i] = -1;
     }
-}
+}// Dijkstra

e2a65556de6919305c4e747f0fe91f0556b6c9d7
sonaspy <sonaspy@zju.edu.cn>
2019-01-23 22:37:36 +0800

    7-3

--- DST_EN/7-3.cpp
+++ DST_EN/7-3.cpp
@@ -1,16 +1,45 @@
-// author - newguo@sonaspy.cn 
-// coding - utf_8 
+// author - newguo@sonaspy.cn
+// coding - utf_8
 
-#include<iostream>
-
-#define test() freopen("in","r",stdin)
+#include <iostream>
+#include <stack>
+#define test() freopen("in", "r", stdin)
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
     /* code */
-    test();
-    
+    //test();
+    int M, N, K;
+    cin >> M >> N >> K;
+    stack<int> S;
+    int arr[10000];
+    while (K--)
+    {
+        while (!S.empty())
+            S.pop();
+        for (int i = 0; i < N; i++)
+            cin >> arr[i];
+        int cur = 0;
+        bool flag = true;
+        for (int i = 0; i < N; i++)
+        {
+            S.push(i + 1);
+            if (S.size() > M)
+            {
+                flag = false;
+                break;
+            }
+            while (!S.empty() && S.top() == arr[cur])
+            {
+                S.pop();
+                cur++;
+            }
+        }
+        if(S.empty() && flag)
+            printf("YES\n");
+        else printf("NO\n");
+    }
     return 0;
 }

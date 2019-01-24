6813092eb00ef3b9b1feb630ed9f96624176ca01
sonaspy <sonaspy@zju.edu.cn>
2019-01-24 15:39:36 +0800

    7-10

--- DST_EN/7-10.cpp
+++ DST_EN/7-10.cpp
@@ -11,48 +11,46 @@ struct node
 {
     double x = 0, y = 0;
     int vis = 0;
-};
+}tmp;
 node ori;
-vector<node> map_cro;
-int n;
-double d;
-double fdis(node a, node b)
+vector<node> cayman;
+int n, d;
+inline double fdis(node a, node b)
 {
     return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
 }
 void dfs(int i)
 {
-    map_cro[i].vis = 1;
-    if (50 - abs(map_cro[i].x) <= d || 50 - abs(map_cro[i].y) <= d)
+    cayman[i].vis = 1;
+    if (50 - abs(cayman[i].x) <= d || 50 - abs(cayman[i].y) <= d)
     {
-        puts("Yes");
+        printf("Yes");
         exit(0);
     }
     for (int j = 0; j < n; j++)
-        if (!map_cro[j].vis && fdis(map_cro[i], map_cro[j]) <= d)
+        if (!cayman[j].vis && fdis(cayman[i], cayman[j]) <= d)
             dfs(j);
 }
 int main()
 {
-    node nd;
-    vector<int> vid;
-    scanf("%d%lf", &n, &d);
+    vector<int> firstJump;
+    scanf("%d%d", &n, &d);
     for (int i = 0; i < n; i++)
     {
-        scanf("%lf%lf", &nd.x, &nd.y);
-        map_cro.push_back(nd);
+        scanf("%lf%lf", &tmp.x, &tmp.y);
+        cayman.push_back(tmp);
     }
     if (7.5 + d >= 50)
     {
-        puts("Yes");
+        printf("Yes");
         return 0;
     }
     for (int i = 0; i < n; i++)
-        if (fdis(map_cro[i], ori) <= d + 7.5)
-            vid.push_back(i); // first jump
-    for (int i = 0; i < vid.size(); i++)
-        if (!map_cro[vid[i]].vis)
-            dfs(vid[i]);
-    puts("No");
+        if (fdis(cayman[i], ori) <= d + 7.5)
+            firstJump.push_back(i); // first jump
+    for (int i = 0; i < firstJump.size(); i++)
+        if (!cayman[firstJump[i]].vis)
+            dfs(firstJump[i]);
+    printf("No");
     return 0;
 }

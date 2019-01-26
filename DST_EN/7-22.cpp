268ae917e1bee9be57a0448ceb1f8d8bcdbbed11
sonaspy <sonaspy@zju.edu.cn>
2019-01-26 15:46:08 +0800

    7-22

---
+++ DST_EN/7-22.cpp
@@ -0,0 +1,21 @@
+// author - newguo@sonaspy.cn 
+// coding - utf_8 
+
+#include<iostream>
+#include<string>
+#define test() freopen("in","r",stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    test();
+    int n,m;
+    string s1, s2;
+    cin >> n >> m;
+    getchar();
+    
+    
+    return 0;
+}

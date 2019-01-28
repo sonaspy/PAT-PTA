72a735b5c0755ae0cdbc8971c7f04d07554e9e14
sonaspy <sonaspy@zju.edu.cn>
2019-01-28 16:38:45 +0800

    3-4

--- DST_ZJU_BOOK/3.4.cpp
+++ DST_ZJU_BOOK/3.4.cpp
@@ -11,7 +11,7 @@ int main(int argc, char const *argv[])
 {
     /* code */
     //test();
-    int n, a[100001], firstCur[100000], lastCur[100000], len[100000], j = 0;
+    int n, a[100001], firstCur[100000], lastCur[100000], len[100000], j = 0, cc = 0, maxlen = 0;
     cin >> n;
     for (int i = 0; i < n; i++)
         scanf("%d", &a[i]);
@@ -22,17 +22,15 @@ int main(int argc, char const *argv[])
         else
         {
             len[j] = lastCur[j] - firstCur[j] + 1;
+            if (len[j] > maxlen)
+            {
+                cc = j;
+                maxlen = len[j];
+            }
             firstCur[++j] = i;
             lastCur[j] = i;
         }
     }
-    int cc, maxlen = -1;
-    for (int i = 0; i < n; i++)
-        if (len[i] > maxlen)
-        {
-            cc = i;
-            maxlen = len[i];
-        }
     for (int i = firstCur[cc]; i <= lastCur[cc]; i++)
     {
         if (i == firstCur[cc])

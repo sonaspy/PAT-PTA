5e13a91c38d70b2116aee081b77e6fc9c3006c4b
sonaspy <sonaspy@zju.edu.cn>
2019-01-28 15:16:00 +0800

    2-3.cpp

---
+++ DST_ZJU_BOOK/2.3.cpp
@@ -0,0 +1,32 @@
+// author - newguo@sonaspy.cn 
+// coding - utf_8 
+
+#include<iostream>
+#include<list>
+#define test() freopen("in","r",stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    //test();
+    list<int> lis;
+    int a, n, carry = 0, sum, num;
+    cin >> a >> n;
+    n++;
+    if(n == 1){
+        cout << 0;
+        return 0;
+    }
+    while(n-- > 1){
+        sum = n * a + carry;
+        num = sum % 10;
+        carry = sum / 10;
+        lis.push_front(num);
+    }
+    if(carry > 0)lis.push_front(carry);
+    for(auto i : lis)
+        cout << i;
+    return 0;
+}

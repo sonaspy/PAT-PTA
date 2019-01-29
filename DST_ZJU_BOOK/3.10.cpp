0024ccc10ba486d10248feb32bbe5627d2190e1f
sonaspy <sonaspy@zju.edu.cn>
2019-01-29 15:50:36 +0800

    3-10

---
+++ DST_ZJU_BOOK/3.10.cpp
@@ -0,0 +1,52 @@
+// author - newguo@sonaspy.cn 
+// coding - utf_8 
+
+#include<iostream>
+#include <map>
+#include <algorithm>
+#include <stack>
+
+#define test() freopen("in","r",stdin)
+using namespace std;
+class abc
+{
+  public:
+    int d;
+    char a, b, c;
+    abc(char x, char y, char z, int s) : a(x), b(y), c(z), d(s) {}
+};
+void hanio(int n, char a, char b, char c)
+{
+    if (n == 1)
+    {
+        cout << a << " -> " << c << endl;
+        return;
+    }
+    hanio(n - 1, a, c, b);
+    hanio(1, a, b, c);
+    hanio(n - 1, b, a, c);
+}
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+    abc temp('a', 'b', 'c', n);
+    stack<abc> q;
+    temp.d = n, temp.a = 'a', temp.b = 'b', temp.c = 'c';
+    q.push(temp);
+    while (!q.empty())
+    {
+        temp = q.top();
+        q.pop();
+        if (temp.d == 1)
+            printf("%c -> %c\n", temp.a, temp.c);
+        else
+        {
+            q.push(abc(temp.b, temp.a, temp.c, temp.d - 1));
+            q.push(abc(temp.a, temp.b, temp.c, 1));
+            q.push(abc(temp.a, temp.c, temp.b, temp.d - 1));
+        }
+    }
+    //    cout<<endl;
+    //    hanio(n,'a','b','c');
+}

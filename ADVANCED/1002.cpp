64ae8b454bb7d81ce4a63c37292e5e01169e8dd6
sonaspy <sonaspy@zju.edu.cn>
2019-03-01 20:22:05 +0800

    1001/1002

--- ADVANCED/1002.cpp
+++ ADVANCED/1002.cpp
@@ -1,9 +1,9 @@
-// author - newguo@sonaspy.cn 
-// coding - utf_8 
+// author - newguo@sonaspy.cn
+// coding - utf_8
 
-#include<iostream>
-#include<queue>
-#define test() freopen("in","r",stdin)
+#include <iostream>
+#include <queue>
+#define test() freopen("in", "r", stdin)
 
 using namespace std;
 
@@ -15,32 +15,39 @@ int main(int argc, char const *argv[])
     double co;
     queue<pair<int, double>> p1, p2, res;
     cin >> k;
-    for(int i = 0; i < k; i ++){
-        scanf("%d%lf",&e,&co);
-        p1.push(make_pair(e,co));
+    for (int i = 0; i < k; i++)
+    {
+        scanf("%d%lf", &e, &co);
+        p1.push(make_pair(e, co));
     }
     cin >> k;
     for (int i = 0; i < k; i++)
     {
         scanf("%d%lf", &e, &co);
-        p2.push(make_pair(e,co));
+        p2.push(make_pair(e, co));
     }
-    while(p1.size() && p2.size()){
-        if(p1.front().first > p2.front().first){
+    while (p1.size() && p2.size())
+    {
+        if (p1.front().first > p2.front().first)
+        {
             res.push(p1.front());
             p1.pop();
         }
-        else if (p1.front().first < p2.front().first){
+        else if (p1.front().first < p2.front().first)
+        {
             res.push(p2.front());
             p2.pop();
         }
-        else{
+        else
+        {
             double x = p1.front().second + p2.front().second;
-            if(x) res.push(make_pair(p1.front().first, x));
-            p1.pop(),p2.pop();
+            if (x)
+                res.push(make_pair(p1.front().first, x));
+            p1.pop(), p2.pop();
         }
     }
-    while(p1.size()){
+    while (p1.size())
+    {
         res.push(p1.front());
         p1.pop();
     }
@@ -50,8 +57,9 @@ int main(int argc, char const *argv[])
         p2.pop();
     }
     cout << res.size();
-    while(res.size()){
-        printf(" %d %.1f",res.front().first,res.front().second);
+    while (res.size())
+    {
+        printf(" %d %.1f", res.front().first, res.front().second);
         res.pop();
     }
     return 0;

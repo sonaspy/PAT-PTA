5e4d32e69307aa90b08dcd57c5ec0ee5e0142f9f
sonaspy <sonaspy@zju.edu.cn>
2019-03-03 21:05:56 +0800

    1008

---
+++ ADVANCED/1008.cpp
@@ -0,0 +1,25 @@
+// author - newguo@sonaspy.cn 
+// coding - utf_8 
+
+#include<iostream>
+
+#define test() freopen("in","r",stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    //test();
+    int useTime = 0, f = 0, n, req;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> req;
+        if(req > f)useTime += (req - f) * 6;
+        else useTime += (f-req) * 4;
+        useTime  += 5;
+        f = req;
+    }
+    cout << useTime;
+    return 0;
+}

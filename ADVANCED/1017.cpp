77ece36830dfe952ad3dd366688311f416ec25d0
sonaspy <sonaspy@zju.edu.cn>
2019-03-08 01:53:53 +0800

    1017

--- ADVANCED/1017.cpp
+++ ADVANCED/1017.cpp
@@ -1,14 +1,14 @@
-// author - newguo@sonaspy.cn 
-// coding - utf_8 
+// author - newguo@sonaspy.cn
+// coding - utf_8
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define test() freopen("in","r",stdin)
+#define test() freopen("in", "r", stdin)
 using namespace std;
 struct node
 {
-    int arrive, time;
+    int arrive, serv_t;
 } tmp;
 bool cmp1(node a, node b)
 {
@@ -21,12 +21,12 @@ int main()
     vector<node> Customers;
     for (int i = 0; i < n; i++)
     {
-        int hh, mm, ss, time;
-        scanf("%d:%d:%d %d", &hh, &mm, &ss, &time);
+        int hh, mm, ss, serv_t;
+        scanf("%d:%d:%d %d", &hh, &mm, &ss, &serv_t);
         int arrive = hh * 3600 + mm * 60 + ss;
         if (arrive > 17 * 3600)
             continue;
-        tmp = {arrive, time * 60};
+        tmp = {arrive, serv_t * 60};
         Customers.push_back(tmp);
     }
     sort(Customers.begin(), Customers.end(), cmp1);
@@ -43,14 +43,14 @@ int main()
                 ti = j;
             }
         }
-        if (window[ti] <= Customers[i].arrive)
-            window[ti] = Customers[i].arrive + Customers[i].time;
-        else
+        if (Customers[i].arrive < window[ti])
         {
             totalWait += (window[ti] - Customers[i].arrive);
-            window[ti] += Customers[i].time;
+            window[ti] += Customers[i].serv_t;
         }
+        else
+            window[ti] = Customers[i].arrive + Customers[i].serv_t;
     }
-    printf("%.1f", totalWait / 60.0 / Customers.size());
+    printf("%.1f", totalWait / (60.0 * Customers.size()));
     return 0;
 }

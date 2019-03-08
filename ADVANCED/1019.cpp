6ee6fcb6085d9294d56e89d637ef7376fde2ca7e
sonaspy <sonaspy@zju.edu.cn>
2019-03-09 05:29:08 +0800

    1019

---
+++ ADVANCED/1019.cpp
@@ -0,0 +1,32 @@
+// author - newguo@sonaspy.cn 
+// coding - utf_8 
+
+#include<iostream>
+#include<string>
+#include<vector>
+#define test() freopen("in","r",stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    //test();
+    vector<int> numbers;
+    int num, b;
+    cin >> num >> b;
+    while (num)
+    {
+        int tmp = num % b;
+        num /= b;
+        numbers.push_back(tmp);
+    }
+    bool f = true;
+    for(int i = 0; i <= numbers.size()/2;i++)
+        if(numbers[i] != numbers[numbers.size()-1 - i]) f = false;
+    cout << (f? "Yes":"No") << endl;
+    cout << numbers[numbers.size()-1];
+    for(int i = numbers.size()-2; i >-1;i--)
+    cout << " " << numbers[i];
+    return 0;
+}

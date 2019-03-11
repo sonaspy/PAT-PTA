33fdb1ccf30140ba236c2ca92c60ccba9bfca788
sonaspy <sonaspy@zju.edu.cn>
2019-03-11 19:29:34 +0800

    1020

--- ADVANCED/1020.cpp
+++ ADVANCED/1020.cpp
@@ -2,7 +2,7 @@
 // coding - utf_8
 
 #include <iostream>
-#include <set>
+#include <map>
 #include <vector>
 #include <algorithm>
 #include <string>
@@ -10,25 +10,16 @@
 
 using namespace std;
 //attention
-struct Node
-{
-    int id, data;
-    bool operator<(const Node &b)const
-    {
-        return id < b.id;
-    }
-};
+
 vector<int> post, in;
-set<Node> res;
+map<int, int> res;
 
 void solve(int root, int left, int right, int id)
 {
     if (left > right) return;
     int i = left;
-    Node tmp;
     while (i < right && in[i] != post[root]) i++;
-    tmp.id = id, tmp.data = post[root];
-    res.insert(tmp);
+    res[id] = post[root];
     solve(root - 1 - right + i, left, i - 1, id * 2 + 1);
     solve(root - 1, i + 1, right, id * 2 + 2);
 }
@@ -44,8 +35,7 @@ int main(int argc, char const *argv[])
     for (int i = 0; i < n; i++) cin >> post[i];
     for (int i = 0; i < n; i++) cin >> in[i];
     solve(n - 1, 0, n - 1, 0);
-    for(auto i : res) s += to_string(i.data) + " ";
-    s.pop_back();
-    cout << s;
+    for(auto i : res) s += to_string(i.second) + " ";
+    s.pop_back(), cout << s;
     return 0;
 }

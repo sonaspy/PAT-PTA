3bdf24cbee7cc750abb70d121852cf074cb06254
sonaspy <sonaspy@zju.edu.cn>
2019-03-12 23:48:40 +0800

    1027

---
+++ ADVANCED/1027.cpp
@@ -0,0 +1,19 @@
+// author - newguo@sonaspy.cn 
+// coding - utf_8 
+
+#include<iostream>
+#include <string>
+#define test() freopen("in","r",stdin)
+using namespace std;
+int main()
+{
+    string c = "0123456789ABC";
+    printf("#");
+    for (int i = 0; i < 3; i++)
+    {
+        int num;
+        scanf("%d", &num);
+        printf("%c%c", c[num / 13], c[num % 13]);
+    }
+    return 0;
+}

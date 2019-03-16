0ad8a9fd34e80ed2eea897e626cfa301ac70e5a7
sonaspy <sonaspy@zju.edu.cn>
2019-03-16 16:46:08 +0800

    update

--- DST_EN/demo.cpp
+++ DST_EN/demo.cpp
@@ -1,31 +1,41 @@
-#include <stdio.h>
+// author - newguo@sonaspy.cn
+// coding - utf_8
 
-void swapv(int a, int b)
+#include <iostream>
+#include <regex>
+#include <string>
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
 {
-    int temp;
-    temp = a;
-    a = b;
-    b = temp;
-}
+    /* code */
+    //test();
+    string str1 = "          sigalhu233";
 
-void swapp(int &a, int &b)
-{
-    int temp;
-    temp = a;
-    a = b;
-    b = temp;
-}
+    regex r("\\s{0,1}");
+    if (regex_match(str1.begin(), str1.begin() + 3, r))
+        cout << "字符串匹配成功！" << endl;
+    else
+        cout << "字符串匹配失败！" << endl;
 
-int main()
-{
+    string str = "sigalhu233sigal233hu233";
+    smatch sm;
 
-    int x = -1, y = 3;
-
-    swapv(x, y);
-    printf("x=%d,y=%d;\n ", x, y);
-
-    swapp(x, y);
-    printf("x=%d,y=%d\n", x, y);
+    cout << "正则表达式：([a-z]+)2" << endl;
+    for (auto it = str.cbegin(); regex_search(it, str.cend(), sm, regex("([a-z]+)2")); it = sm.suffix().first)
+    {
+        cout << "字符串：" << &*it << " 匹配成功！" << endl;
+        cout << "匹配字符子串个数：" << sm.size() << endl;
+        cout << "分别为：";
+        for (auto aa : sm)
+            cout << aa.str() << " ";
+        cout << endl;
+        cout << "字符串 " << sm.str() << " 前的字符串为：" << sm.prefix().str() << endl;
+        cout << "字符串 " << sm.str() << " 后的字符串为：" << sm.suffix().str() << endl;
+        cout << endl;
+    }
 
     return 0;
 }
---
+++ PRIMER/cpp_test.cpp
@@ -0,0 +1,51 @@
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <iostream>
+#include <time.h>
+#include <string>
+#include <unistd.h>
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+string replace_all_distinct(string &str1, const string &old_value, const string &new_value)
+{
+    string str = str1;
+    for (string::size_type pos(0); pos != string::npos; pos += new_value.length())
+    {
+        if ((pos = str.find(old_value, pos)) == string::npos)
+            break;
+        str.replace(pos, old_value.length(), new_value);
+    }
+    return str;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+
+    string s = "huojiandui";
+    string news = replace_all_distinct(s,"huo","yytrt");
+    cout << news;
+    // double start = clock();
+    // // Place your codes here...
+    // long long count = 0;
+    // for (long long i = 0; i < 4999999999; i++)
+    //     count++;
+
+    // double ends = clock();
+
+    // cout << "post-self-increment Running Time : " << (double)(ends - start) / CLOCKS_PER_SEC << " s" << endl;
+
+    // start = clock();
+    // // Place your codes here...
+    // count = 0;
+    // for (long long i = 0; i < 4999999999; i++)
+    //     ++count;
+
+    // ends = clock();
+
+    // cout << "pre-self-increment Running Time : " << (double)(ends - start) / CLOCKS_PER_SEC << " s" << endl;
+    return 0;
+}

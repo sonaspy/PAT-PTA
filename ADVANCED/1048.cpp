8afdcb054b79b3e3e75eed2c12121d84ea1aebd6
sonaspy <sonaspy@zju.edu.cn>
2019-03-22 14:56:36 +0800

    1048

---
+++ ADVANCED/1048.cpp
@@ -0,0 +1,32 @@
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <iostream>
+
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+
+int n, m, tmp, mp[1005]; // if you do not decare variables(especially the array mp[1005]) as global, test case 3 won't pass.
+int main(int argc, char const *argv[])
+{
+    /* code */
+    //test();
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &tmp);
+        ++mp[tmp];
+    }
+    for (int i = 1; i < m; i++)
+    {
+        if (mp[i] && mp[m - i])
+        {
+            if (2 * i == m && mp[i] < 2) continue;
+            printf("%d %d\n", i, m - i);
+            return 0;
+        }
+    }
+    printf("No Solution");
+    return 0;
+}

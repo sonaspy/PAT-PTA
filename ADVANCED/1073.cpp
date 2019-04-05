f716a6e3c5f61f8452c03952e83e0dde1931d756
sonaspy <sonaspy@zju.edu.cn>
2019-04-05 12:35:43 +0800

    update

--- ADVANCED/1073.cpp
+++ ADVANCED/1073.cpp
@@ -9,31 +9,30 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     /* code */
-    test();
     string s, ans;
     cin >> s;
-    if (s[0] == '-') ans += '-';
+    if (s[0] == '-')
+        ans = "-";
     int indexE = s.find("E");
     string num = s.substr(1, indexE - 1);
     char x = s[indexE + 1];
-    cout << indexE <<endl;
     string exp = s.substr(indexE + 2, s.size() - indexE - 2);
-    int e = stoi(exp);
+    int E = stoi(exp);
     if (x == '+')
-    { 
-        if (e < num.size() - 2)
-            ans += num[0] + num.substr(2, e) + "." + num.substr(e + 2, num.size() - e - 2);
+    {
+        if (E < num.size() - 2)
+            ans += num[0] + num.substr(2, E) + "." + num.substr(E + 2, num.size() - E - 2);
         else
         {
-            num.erase(num.begin()+1), ans += num;
-            for (int i = 0; i < e - (num.size() -1) ; i++)
-                ans += "0";
+            num.erase(num.begin() + 1), ans += num;
+            for (int i = 0; i < E - (num.size() - 1); i++)
+                ans.push_back('0');
         }
     }
-    if (x == '-')
-    { 
+    else
+    {
         ans += "0.";
-        while (--e) ans += "0";
+        while (--E) ans.push_back('0');
         num.erase(num.begin() + 1), ans += num;
     }
     cout << ans << endl;

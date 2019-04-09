e356f2ea486ec6a0ffad5df84c4078a07e334207
sonaspy <sonaspy@zju.edu.cn>
2019-04-09 15:20:21 +0800

    update

---
+++ ADVANCED/1084.cpp
@@ -0,0 +1,36 @@
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <bits/stdc++.h>
+
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    //test();
+    string s1, s2, s11, s22, res;
+    cin >> s1 >> s2;
+    unordered_set<char> st;
+    transform(s1.begin(), s1.end(), s1.begin(), ::toupper);
+    transform(s2.begin(), s2.end(), s2.begin(), ::toupper);
+    for (auto i : s1)
+    {
+        if (!st.count(i)) s11.push_back(i);
+        st.insert(i);
+    }
+    st.clear();
+    for (auto i : s2)
+    {
+        st.insert(i);
+    }
+    for (auto i : s11)
+    {
+        if (!st.count(i))
+            res.push_back(i);
+    }
+    cout << res;
+    return 0;
+}
--- ADVANCED/tempCodeRunnerFile.cpp
+++ ADVANCED/tempCodeRunnerFile.cpp
@@ -1,7 +1,49 @@
-l = v.front();
-            // if (l != a1 && l != a2)
-            //     printf("LCA of %d and %d is %d.\n", a1, a2, l);
-            // else if (l == a1)
-            //     printf("%d is an ancestor of %d.\n", a1, a2);
-            // else
-            //     printf("%d is an ancestor of %d.\n", a2, a1);
+// author - newguo@sonaspy.cn
+// coding - utf_8
+
+#include <bits/stdc++.h>
+
+#define test() freopen("in", "r", stdin)
+
+using namespace std;
+typedef struct Student *pstu;
+struct Student
+{
+    string name, id;
+    int grade;
+    Student(char n[], char i[], int g) : name(n), id(i), grade(g) {}
+};
+bool cmp(const pstu a, const pstu b)
+{
+    return a->grade > b->grade;
+}
+int main(int argc, char const *argv[])
+{
+    /* code */
+    test();
+    int n, g1, g2, g;
+    cin >> n;
+    vector<pstu> stus, res;
+    for (int i = 0; i < n; i++)
+    {
+        char name[12], id[12];
+        scanf("%s %s %d", name, id, &g);
+        stus.push_back(new Student(name, id, g));
+    }
+    cin >> g1 >> g2;
+    for (auto i : stus)
+    {
+        if (i->grade >= g1 && i->grade <= g2)
+            res.push_back(i);
+    }
+    if (res.empty())
+    {
+        cout << "NONE";
+        return 0;
+    }
+    sort(res.begin(), res.end(), cmp);
+    for (auto i : res)
+        printf("%s %s\n", i->name.c_str(), i->id.c_str());
+
+    return 0;
+}

e622ad8b00143ec35dfca2000ee11fcaad5cbd1c
sonaspy <sonaspy@zju.edu.cn>
2019-04-17 00:36:10 +0800

    update

--- PRIMER/demo1.cpp
+++ PRIMER/demo1.cpp
@@ -6,21 +6,12 @@
 #define test() freopen("in", "r", stdin)
 
 using namespace std;
-struct country
-{
-    double gold, medal, g_p, m_p;
-    int rank[5];
-};
+
 int main(int argc, char const *argv[])
 {
     /* code */
     //test();
-    country cons[10];
-    int id = 1;
-    int i = 10;
-    cons[1].medal = 10;
-    cons[1].gold = 1;
-    int  offset = offsetof(country, medal);
-    cout << (*((double *)&cons[1] + id - 1) == *((double *)&cons[2] + id - 1));
+    int x= 1;
+    cout << (x == (int)(float)x);
     return 0;
 }

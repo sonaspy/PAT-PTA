e15d600edc356961f64909319dde192849b36d20
sonaspy <sonaspy@zju.edu.cn>
2019-04-28 17:47:25 +0800

    update

--- ADVANCED/1020.cpp
+++ ADVANCED/1020.cpp
@@ -1,16 +1,12 @@
-// author - newguo@sonaspy.cn
-// coding - utf_8
+// author - newguo@sonaspy.cn 
+// coding - utf_8 
 
-#include <iostream>
-#include <map>
-#include <vector>
-#include <algorithm>
-#include <string>
-#define test() freopen("in", "r", stdin)
+#include<bits/stdc++.h>
+
+#define test() freopen("in","r",stdin)
 
 using namespace std;
 //attention
-
 vector<int> post, in;
 map<int, int> res;
 
@@ -23,19 +19,6 @@ void solve(int root, int lo, int hi, int id)
     solve(root - 1 + i - hi, lo, i - 1, id * 2 + 1);
     solve(root - 1, i + 1, hi, id * 2 + 2);
 }
-
-// pre , in -> level
-// void solve(int root, int lo, int hi, int id)
-// {
-//     if (hi < lo)
-//         return;
-//     int i = lo;
-//     while (i < hi && in[i] != pre[root])
-//         i++;
-//     res[id] = pre[root];
-//     solve(root + 1, lo, i - 1, id * 2 + 1);
-//     solve(root + 1 + i - lo, i + 1, hi, id * 2 + 2);
-// }
 
 int main(int argc, char const *argv[])
 {

7f283796f3ecd49c9567220ad418df140bfeca81
sonaspy <sonaspy@zju.edu.cn>
2019-05-10 01:10:35 +0800

    update

--- ADVANCED/1154.cpp
+++ ADVANCED/1154.cpp
@@ -1,15 +1,46 @@
-// author - newguo@sonaspy.cn 
-// coding - utf_8 
+// author - newguo@sonaspy.cn
+// coding - utf_8
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
-#define test() freopen("in","r",stdin)
+#define test() freopen("in", "r", stdin)
 
 using namespace std;
-
+vector<int> MAP[10010];
+unordered_set<int> Color;
 int main(int argc, char const *argv[])
 {
     /* code */
     //test();
+    int n, m, v1, v2, k, c;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%d%d", &v1, &v2);
+        MAP[v1].push_back(v2), MAP[v2].push_back(v1);
+    }
+    cin >> k;
+    while (k--)
+    {
+        vector<int> vc(n);
+        Color.clear();
+        bool flag = true;
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &vc[i]);
+            Color.insert(vc[i]);
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (auto j : MAP[i])
+                if (vc[j] == vc[i])
+                {
+                    flag = false;
+                    goto end;
+                }
+        }
+    end:
+        !flag ? printf("No\n") : printf("%lu-coloring\n", Color.size());
+    }
     return 0;
 }
--- ADVANCED/1155.cpp
+++ ADVANCED/1155.cpp
@@ -1,15 +1,40 @@
-// author - newguo@sonaspy.cn 
-// coding - utf_8 
+// author - newguo@sonaspy.cn
+// coding - utf_8
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
-#define test() freopen("in","r",stdin)
+#define test() freopen("in", "r", stdin)
 
 using namespace std;
-
+int n, myheap[1111] = {0};
+vector<int> path;
+inline void output()
+{
+    for (int i = 0; i < path.size(); i++)
+        printf("%d%c", path[i], i == path.size() - 1 ? '\n' : ' ');
+}
+bool order(int id)
+{
+    if (id >= n) return false;
+    path.push_back(myheap[id]);
+    bool f1 = order(id * 2 + 2), f2 = order(id * 2 + 1);
+    if (!f1 && !f2) output();
+    path.pop_back();
+    return true;
+}
 int main(int argc, char const *argv[])
 {
     /* code */
     //test();
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        scanf("%d", myheap + i);
+    order(0);
+    if (is_heap(myheap, myheap + n, greater<int>()))
+        printf("Min Heap\n");
+    else if (is_heap(myheap, myheap + n, less<int>()))
+        printf("Max Heap\n");
+    else
+        printf("Not Heap\n");
     return 0;
 }

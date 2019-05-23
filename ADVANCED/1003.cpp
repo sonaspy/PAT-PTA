d55151c6434f86d1de1b3e24342e68f3b81d14a1
sonaspy <sonaspy@zju.edu.cn>
2019-05-23 19:19:25 +0800

    update

--- ADVANCED/1003.cpp
+++ ADVANCED/1003.cpp
@@ -11,18 +11,18 @@ int main(int argc, char const *argv[])
 {
     /* code */
     //test();
-    int n, m, c1, c2, a1, a2, b, teams[501], map[501][501], W[501], dis[501], pathnum[501];
+    int n, m, c1, c2, a1, a2, b, team[501], map[501][501], sumTeam[501], dis[501], pathnum[501];
     bool vis[501];
     cin >> n >> m >> c1 >> c2;
     for (int i = 0; i < n; i++)
-        scanf("%d", &teams[i]);
+        scanf("%d", &team[i]);
     fill(map[0], map[0] + 501 * 501, INF), fill(dis, dis + 501, INF), fill(vis, vis + 501, false);
     for (int i = 0; i < m; i++)
     {
         scanf("%d%d%d", &a1, &a2, &b);
         map[a1][a2] = map[a2][a1] = b;
     }
-    dis[c1] = 0, W[c1] = teams[c1], pathnum[c1] = 1;
+    dis[c1] = 0, sumTeam[c1] = team[c1], pathnum[c1] = 1;
     while (true)
     {
         int k = -1, _min = INF;
@@ -45,17 +45,17 @@ int main(int argc, char const *argv[])
                 {
                     dis[v] = dis[k] + map[k][v];
                     pathnum[v] = pathnum[k];
-                    W[v] = W[k] + teams[v];
+                    sumTeam[v] = sumTeam[k] + team[v];
                 }
                 else if (dis[k] + map[k][v] == dis[v])
                 {
                     pathnum[v] += pathnum[k];
-                    if (W[v] < W[k] + teams[v])
-                        W[v] = W[k] + teams[v];
+                    if (sumTeam[v] < sumTeam[k] + team[v])
+                        sumTeam[v] = sumTeam[k] + team[v];
                 }
             }
         }
     } //dijkstra
-    printf("%d %d", pathnum[c2], W[c2]);
+    printf("%d %d", pathnum[c2], sumTeam[c2]);
     return 0;
 }
--- ADVANCED/1014.cpp
+++ ADVANCED/1014.cpp
@@ -1,46 +1,40 @@
-// author -  newguo@sonaspy.cn
+// author - newguo@sonaspy.cn
 // coding - utf_8
 
-#include <iostream>
-#include <algorithm>
-#include <queue>
+#include <bits/stdc++.h>
 #define test() freopen("in", "r", stdin)
-
 using namespace std;
-#define INF 99999999
-int n, m, k, q, T[1001], finish[1001], nowTime = 8 * 60;
+#define INF 1 << 30
+int n, m, k, q, serveT[1001], finish[1001], cur_time = 8 * 60, id;
 queue<pair<int, int>> Q[20];
-queue<int> waiting;
-
-void solve(int value)
+queue<int> beyondLine;
+void Enline(int t)
 {
-    while (waiting.size())
+    while (beyondLine.size())
+    {
+        int k = -1, min_size = m, tmp;
+        for (int i = 0; i < n; i++)
         {
-            int k = -1, min = m, tmp;
-            for (int i = 0; i < n; i++)
-                {
-                    if (Q[i].size() < min)
-                        {
-                            min = Q[i].size();
-                            k = i;
-                        }
-                }
-            if (k == -1)
-                return;
-            Q[k].push(make_pair(waiting.front(), T[waiting.front()])), waiting.pop();
+            if (Q[i].size() < min_size)
+            {
+                min_size = Q[i].size();
+                k = i;
+            }
         }
-    if (waiting.empty())
+        if (k == -1)
+            return;
+        Q[k].push(make_pair(beyondLine.front(), serveT[beyondLine.front()])), beyondLine.pop();
+    }
+    if (beyondLine.empty())
+        for (int i = 0; i < n; i++)
         {
-            for (int i = 0; i < n; i++)
-                {
-                    nowTime = value;
-                    while (Q[i].size())
-                        {
-                            nowTime += Q[i].front().second;
-                            finish[Q[i].front().first] = nowTime;
-                            Q[i].pop();
-                        }
-                }
+            cur_time = t;
+            while (Q[i].size())
+            {
+                cur_time += Q[i].front().second;
+                finish[Q[i].front().first] = cur_time;
+                Q[i].pop();
+            }
         }
 }
 
@@ -50,47 +44,43 @@ int main(int argc, char const *argv[])
     //test();
     cin >> n >> m >> k >> q;
     for (int i = 1; i <= k; i++)
+    {
+        scanf("%d", &serveT[i]);
+        beyondLine.push(i);
+    }
+    while (beyondLine.size())
+    {
+        int first_dq = INF, k = -1;
+        for (int i = 0; i < n; i++)
         {
-            scanf("%d", &T[i]);
-            waiting.push(i);
+            if (Q[i].size() && Q[i].front().second < first_dq)
+            {
+                first_dq = Q[i].front().second;
+                k = Q[i].front().first;
+            }
         }
-    while (waiting.size())
+        if (k != -1)
         {
-            int mint = INF, k = -1;
+            cur_time += first_dq;
             for (int i = 0; i < n; i++)
+            {
+                if (Q[i].size())
                 {
-                    if (Q[i].size() && Q[i].front().second < mint)
-                        {
-                            mint = Q[i].front().second;
-                            k = Q[i].front().first;
-                        }
+                    Q[i].front().second -= first_dq;
+                    if (Q[i].front().second == 0)
+                    {
+                        finish[Q[i].front().first] = cur_time;
+                        Q[i].pop();
+                    }
                 }
-            if (k > -1)
-                {
-                    nowTime += mint;
-                    for (int i = 0; i < n; i++)
-                        {
-                            if (Q[i].size())
-                                {
-                                    Q[i].front().second -= mint;
-                                    if (Q[i].front().second == 0)
-                                        {
-                                            finish[Q[i].front().first] = nowTime;
-                                            Q[i].pop();
-                                        }
-                                }
-                        }
-                }
-            solve(nowTime);
+            }
         }
+        Enline(cur_time);
+    }
     for (int i = 0; i < q; i++)
-        {
-            int id;
-            cin >> id;
-            if (finish[id] - T[id] >= 17 * 60)
-                printf("Sorry\n");
-            else
-                printf("%02d:%02d\n", finish[id] / 60, finish[id] % 60);
-        }
+    {
+        cin >> id;
+        finish[id] - serveT[id] >= 17 * 60 ? printf("Sorry\n") : printf("%02d:%02d\n", finish[id] / 60, finish[id] % 60);
+    }
     return 0;
 }

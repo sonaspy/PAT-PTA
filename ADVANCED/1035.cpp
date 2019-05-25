5a9ad7197383859f41dd1eeea5f61b4a5fbacf3d
sonaspy <sonaspy@zju.edu.cn>
2019-05-25 16:49:46 +0800

    update

--- ADVANCED/1035.cpp
+++ ADVANCED/1035.cpp
@@ -1,19 +1,19 @@
 // author - newguo@sonaspy.cn
 // coding - utf_8
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include<unordered_map>
+#include <bits/stdc++.h>
+
 #define test() freopen("in", "r", stdin)
 
 using namespace std;
-struct Node{
+
+struct Node
+{
     string user, pwd;
     bool isMod;
 };
 vector<Node> usrList;
-unordered_map<char,char> mp;
+unordered_map<char, char> mp;
 int main(int argc, char const *argv[])
 {
     /* code */
@@ -25,12 +25,12 @@ int main(int argc, char const *argv[])
     {
         string s, s1;
         cin >> s >> s1;
-        usrList.push_back({s,s1,false});
+        usrList.push_back({s, s1, false});
     }
     int isMod = 0;
-    for (int i = 0; i < usrList.size();i++)
+    for (int i = 0; i < usrList.size(); i++)
     {
-        for (int j = 0; j <usrList[i].pwd.size(); j++)
+        for (int j = 0; j < usrList[i].pwd.size(); j++)
         {
             if (mp.count(usrList[i].pwd[j]))
             {
@@ -38,14 +38,17 @@ int main(int argc, char const *argv[])
                 usrList[i].pwd[j] = mp[usrList[i].pwd[j]];
             }
         }
-        if(usrList[i].isMod) isMod++;
+        if (usrList[i].isMod)
+            isMod++;
     }
-    if(isMod){
-    cout << isMod << endl;
-        for(auto i : usrList)
-            if(i.isMod)
-                printf("%s %s\n",i.user.c_str(),i.pwd.c_str());
+    if (isMod)
+    {
+        cout << isMod << endl;
+        for (auto i : usrList)
+            if (i.isMod)
+                printf("%s %s\n", i.user.c_str(), i.pwd.c_str());
     }
-    else cout << "There "<< (n > 1?"are ":"is ")<< n << " account" << (n > 1? "s":"") << " and no account is modified";
+    else
+        cout << "There " << (n > 1 ? "are " : "is ") << n << " account" << (n > 1 ? "s" : "") << " and no account is modified";
     return 0;
 }

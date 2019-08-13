a11bea08537a0b4b27d73ecbf418f83dbb6ddf04
sonaspy <sonaspy@zju.edu.cn>
2019-08-13 11:00:52 +0800

    update

--- ADVANCED/1086.cpp
+++ ADVANCED/1086.cpp
@@ -19,30 +19,25 @@ enum operation
 {
     push,
     pop,
-    null
+    firstop
 };
-
 void post(ptrNode root)
 {
-    if (!root)
-        return;
-    if (root->left)
-        post(root->left);
-    if (root->right)
-        post(root->right);
-    if(!first) cout << " ";
+    if (!root) return;
+    post(root->left);
+    post(root->right);
+    if (!first) cout << " ";
     cout << root->data;
     first = 0;
 }
-
 int main(int argc, char const *argv[])
 {
     /* code */
     int n, id;
-    operation lastop = null;
-    stack<ptrNode> sk;
+    operation _op = firstop;
+    stack<ptrNode> S;
     cin >> n;
-    ptrNode lastpop, root, TreeNodes[n + 1];
+    ptrNode lastPOP, root, TreeNodes[n + 1];
     string s;
     for (int i = 1; i <= n; i++)
         TreeNodes[i] = new Node(i);
@@ -52,20 +47,20 @@ int main(int argc, char const *argv[])
         if (s[1] == 'u')
         {
             cin >> id;
-            if (lastop == pop)
-                lastpop->right = TreeNodes[id];
-            else if (lastop == push)
-                sk.top()->left = TreeNodes[id];
+            if (_op == pop)
+                lastPOP->right = TreeNodes[id];
+            else if (_op == push)
+                S.top()->left = TreeNodes[id];
             else
                 root = TreeNodes[id];
-            sk.push(TreeNodes[id]);
-            lastop = push;
+            S.push(TreeNodes[id]);
+            _op = push;
         }
         else
         {
-            lastpop = sk.top();
-            sk.pop();
-            lastop = pop;
+            lastPOP = S.top();
+            S.pop();
+            _op = pop;
         }
     }
     post(root);
--- demo1.c
+++ demo1.c
@@ -1,22 +1,11 @@
 #include <stdio.h>
-
 typedef FILE *file_p;
 int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+extern int b;
 int main(int argc, char const *argv[])
 {
     /* code */
     //test();
-    int b[10];
-    file_p fp = fopen("demo.dat", "wb+");
-    fwrite(a, sizeof(int), 10, fp);
-    fclose(fp);
-
-    fp = fopen("demo.dat", "rb+");
-    fread(b, sizeof(int), 10, fp);
-    for (int i = 0; i < 10; i++)
-        printf("%d ", b[i]);
-    fclose(fp);
-
+    printf("%d", b);
     return 0;
 }
-

3ddf1e95652c8a45abcd064aa801caa426c9751a
sonaspy <sonaspy@zju.edu.cn>
2019-08-13 20:21:28 +0800

    update

--- demo1.c
+++ demo1.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
-typedef FILE *file_p;
-int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-extern int b;
+#include <stdlib.h>
+
 int main(int argc, char const *argv[])
 {
     /* code */
-    //test();
+    FILE *fpin, *fpout;
+    int x;
+    if ((fpin = fopen("input.txt", "r")) == NULL)
+    {
+        printf("Can't open file input.txt\n");
+        exit(0);
+    }
+    if ((fpout = fopen("output.txt", "w")) == NULL)
+    {
+        printf("Can't creat file output.txt");
+        exit(0);
+    }
+    fclose(fpin);
     return 0;
 }

984c734e8d642013107524ba2c7882f6c937504b
sonaspy <sonaspy@zju.edu.cn>
2019-08-15 18:38:11 +0800

    update

--- demo1.c
+++ demo1.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
     /* code */
-    FILE *fpin, *fpout;
-    int x;
-    if ((fpin = fopen("input.txt", "r")) == NULL)
-    {
-        printf("Can't open file input.txt\n");
-        exit(0);
-    }
-    if ((fpout = fopen("output.txt", "w")) == NULL)
-    {
-        printf("Can't creat file output.txt");
-        exit(0);
-    }
-    fclose(fpin);
+
     return 0;
 }
--- demo1.cpp
+++ demo1.cpp
@@ -12,99 +12,16 @@ struct TreeNode
     int val;
     TreeNode *left, *right;
 };
-vector<int> preorderTraversal(TreeNode *root)
+void SWAP(int &a, int &b)
 {
-    vector<int> resSeq;
-    if (!root)
-        return resSeq;
-    stack<pair<TreeNode *, bool>> s;
-    s.push(make_pair(root, false));
-    bool isMyTurn;
-    while (s.size())
-    {
-        root = s.top().first, isMyTurn = s.top().second, s.pop();
-        if (isMyTurn)
-            resSeq.push_back(root->val);
-        else
-        {
-            if (root->right)
-                s.push(make_pair(root->right, false));
-            if (root->left)
-                s.push(make_pair(root->left, false));
-            s.push(make_pair(root, true));
-        }
-    }
-    return resSeq;
-}
-vector<int> preorder(TreeNode *root)
-{
-    vector<int> resSeq;
-    if (!root)
-        return resSeq;
-    stack<TreeNode *> s;
-    s.push(root);
-    while (s.size())
-    {
-        root = s.top(), s.pop();
-        resSeq.push_back(root->val);
-        if (root->right)
-            s.push(root->right);
-        if (root->left)
-            s.push(root->left);
-    }
-    return resSeq;
-}
-vector<int> inorder(TreeNode *root)
-{
-    vector<int> resSeq;
-    if (!root)
-        return resSeq;
-    stack<pair<TreeNode *, bool>> s;
-    s.push(make_pair(root, false));
-    bool isMyTurn;
-    while (s.size())
-    {
-        root = s.top().first, isMyTurn = s.top().second, s.pop();
-        if (isMyTurn)
-            resSeq.push_back(root->val);
-        else
-        {
-            if (root->right)
-                s.push(make_pair(root->right, false));
-            s.push(make_pair(root, true));
-            if (root->left)
-                s.push(make_pair(root->left, false));
-        }
-    }
-    return resSeq;
-}
-vector<int> postorder(TreeNode *root)
-{
-    vector<int> resSeq;
-    if (!root)
-        return resSeq;
-    stack<pair<TreeNode *, bool>> s;
-    s.push(make_pair(root, false));
-    bool isMyTurn;
-    while (s.size())
-    {
-        root = s.top().first, isMyTurn = s.top().second, s.pop();
-        if (isMyTurn)
-            resSeq.push_back(root->val);
-        else
-        {
-            s.push(make_pair(root, true));
-            if (root->right)
-                s.push(make_pair(root->right, false));
-            if (root->left)
-                s.push(make_pair(root->left, false));
-        }
-    }
-    return resSeq;
+    a ^= b ^= a ^= b;
 }
 int main(int argc, char const *argv[])
 {
     /* code */
     //test();
+    int a = 10, b = 100;
+    SWAP(a, b);
+    cout << a << b;
     return 0;
 }

20e7cdf2d909fa49ee53f58fe645c524bec7ba99
sonaspy <sonaspy@zju.edu.cn>
2019-09-01 20:50:53 +0800

    update

--- ADVANCED/1136.cpp
+++ ADVANCED/1136.cpp
@@ -6,29 +6,30 @@
 #define test() freopen("in", "r", stdin)
 
 using namespace std;
-typedef long long ll;
-inline bool ispalin(string s)
+inline bool ispalindromic(string s)
 {
-    string s1 = s;
-    reverse(s1.begin(), s1.end());
-    return s1 == s;
+    auto lo = s.begin(), hi = s.end();
+    for (; lo < --hi; ++lo)
+        if (*lo != *hi)
+            return false;
+    return true;
 }
 int main(int argc, char const *argv[])
 {
     /* code */
     //test();
-    ll num, num2, i;
+    int64_t num, num2, i;
     string n;
     cin >> n;
-    for (i = 0; !ispalin(n) && i < 10; i++)
+    for (i = 0; !ispalindromic(n) && i < 10; i++)
     {
         num = stoll(n);
-        string n2(n);
-        reverse(n2.begin(), n2.end());
-        num2 = stoll(n2);
-        printf("%s + %s = %s\n", n.c_str(), n2.c_str(), to_string(num + num2).c_str());
+        string tmp(n);
+        reverse(tmp.begin(), tmp.end());
+        num2 = stoll(tmp);
+        printf("%s + %s = %s\n", n.c_str(), tmp.c_str(), to_string(num + num2).c_str());
         n = to_string(num + num2);
     }
-    ispalin(n) ? printf("%s is a palindromic number.\n", n.c_str()) : printf("Not found in 10 iterations.");
+    ispalindromic(n) ? printf("%s is a palindromic number.\n", n.c_str()) : printf("Not found in 10 iterations.");
     return 0;
 }
